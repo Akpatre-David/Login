e4fe54075a35062d1c924ba2685d40c1cb9a74e2
Akpatre-David <akpatredavid17@gmail.com>
2024-04-10 10:18:48 +0100

    first commit

---
+++ src/component/Signin.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+const Login = () => {
+    return (
+      <>
+        <p> SignIn details</p>
+
+        <form action="">
+          <input type="text" placeholder="Input username" />
+
+                <input type="pssword" placeholder="Input passpord" />
+                
+
+                <button type="submit">send</button>
+        </form>
+      </>
+    );
+}
+ 
+export default Login;
+
